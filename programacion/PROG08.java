3318b419d166009470bcb1f563843feb927b9baf
JavierFeFe <44543081+JavierFeFe@users.noreply.github.com>
2019-04-25 16:13:09 +0200

    Update PROG08.java

--- programacion/PROG08.java
+++ programacion/PROG08.java
@@ -32,26 +32,34 @@ public class PROG08 {
         JsonObject root = new JsonObject(); //Creo el elemento raiz
         JsonObject datosCliente = new JsonObject(); //Creo el subelemento datosCliente
         root.add("datos_cliente", datosCliente); //Añado el elemento al elemento raiz
-        Scanner teclado = new Scanner(System.in); //Capturo una línea de texto
-        System.out.print("Introduce el texto separado por comas: ");
-        String texto = teclado.nextLine();
-        //String texto="X12345678F,\"nombre\",\"apellidos\",+(82)12345678, 612345678,test@TEST.com,(91)23456789 ,prueba@prueba.com, prueba@prueba.com, adsfasdf"; //Texto de ejemplo
+        //Scanner teclado = new Scanner(System.in); //Capturo una línea de texto
+        //System.out.print("Introduce el texto separado por comas: ");
+        //String texto = teclado.nextLine();
+        String texto="X12345678F,\"nombre\",\"apellidos\",+(82)12345678, 612345678,test@TEST.com,(91)23456789 ,prueba@prueba.com, prueba@prueba.com, adsfasdf"; //Texto de ejemplo
         List<String> cadena = new ArrayList<String>(Arrays.asList(texto.split(",")));
         if (cadena.size() >= 3) { //Si la cadena contiene al menos los 3 primeros campos
-
-            //La detección de los 3 primeros campos se podría mejorar mediante los regex adecuados, pero preferí omitirlo ya que
-            //aumentaría considerablemente el código y en las instrucciones del ejercio no especifica que sea necesaria esta comprobación.
-
-            String nif = cadena.get(0).trim(); //Establezco el valor del campo nif
-            String nombre = cadena.get(1).replaceAll("\"", "").trim(); //Establezco el valor del campo nombre, eliminando espacios al principio y final y las comillas
-            String apellidos = cadena.get(2).replaceAll("\"", "").trim(); //Establezco el valor del campo apellidos
+            List<String> errores = new ArrayList<>(); //Creo un listado para los errores
             List<String> telefonos = new ArrayList(); //Creo un listado para los teléfonos
             List<String> telefonosInternacionales = new ArrayList(); //Creo un listado para los teléfonos internacionales
             List<String> correos = new ArrayList(); //Creo un listado para los correos
-            List<String> errores = new ArrayList<>();
-            datosCliente.addProperty("id", nif); //Almaceno el nif en el json
-            datosCliente.addProperty("nombre", nombre); //Almaceno el nombre en el json
-            datosCliente.addProperty("apellidos", apellidos); //Almaceno los apellidos en el json
+            String nif = cadena.get(0).trim(); //Establezco el valor del campo nif
+            if (nif.matches("^[XYxy]?\\d{8}[A-Za-z]$")){//Regex para identificar un DNI o NIE válido
+                datosCliente.addProperty("id", nif); //Almaceno el nif en el json
+            }else{
+                errores.add("NIF Inválido: " + nif);
+            }
+            String nombre = cadena.get(1).replaceAll("\"", "").trim(); //Establezco el valor del campo nombre, eliminando espacios al principio y final y las comillas
+            if (nombre.matches("^[A-Za-zÁÉÍÓÚáéíóúÜü\\s]{2,}$")){ //Regex para identificar una cadena de texto válida para un nombre
+                datosCliente.addProperty("nombre", nombre); //Almaceno el nombre en el json
+            }else{
+                errores.add("Nombre Inválido: " + nombre);
+            }
+            String apellidos = cadena.get(2).replaceAll("\"", "").trim(); //Establezco el valor del campo apellidos
+            if (apellidos.matches("^[A-Za-zÁÉÍÓÚáéíóúÜü\\s]{2,}+$")){//Mismo regex que para el nombre
+                datosCliente.addProperty("apellidos", apellidos); //Almaceno el nombre en el json
+            }else{
+                errores.add("Apellidos Inválidos: " + apellidos);
+            }
             for (int i = 3; i < cadena.size() ; i++) { //Recorro todos los campos de la cadena
                 String campo = cadena.get(i);
                 if (campo.trim().matches("^\\+?(\\(\\d+\\))?\\d{5}\\d*$")) { //Interpreto mediante un regex un nº de teléfono válido
@@ -71,7 +79,7 @@ public class PROG08 {
                         }
                     }
                     //Podría hacer simplemente un else si elimino los 3 primeros campos de la cadena, pero mediante este regex me aseguro de su correcta interpretación
-                } else if (campo.trim().matches("^.+@.+\\..+$")) { //Interpreto mediante un regex el campo de correo electrónico
+                } else if (campo.trim().matches("^\\w+@\\w+\\.\\w{1,4}(\\.\\w{1,4})?$")) { //Interpreto mediante un regex el campo de correo electrónico
                     String correo = campo.trim().toLowerCase();
                     if (!correos.contains(correo)) {
                         correos.add(correo);
@@ -120,3 +128,4 @@ public class PROG08 {
         }
     }
 }
+
